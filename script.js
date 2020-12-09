fd2bcfeb7bff5fbde5e9b5549b024bda08c9b5f3
EthanPrieur <ethan.prieur@ocsbstudent.ca>
2020-12-09 15:47:49 +0000

    Removed a segment that messed up another segment.

--- script.js
+++ script.js
@@ -2,13 +2,3 @@ function myFunction() {
   var x = document.getElementById("myText").value;
   document.getElementById("demo").innerHTML = x;
 }
-var p1 = 3;
-var p2 = 8;
-document.getElementById("button2").addEventListener("click", function() {
-  myFunction(p1, p2);
-});
-
-function myFunction(a, b) {
-  var answer = a * b;
-  document.getElementById("bonus").innerHTML = answer;
-}
